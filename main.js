ea99d7513205cf4144ded6fea1bcda364411ce48
Debora Cantuarias <d.cantuarias.urrutia@gmail.com>
2024-08-05 22:36:44 -0400

    adding css + js elements

---
+++ main.js
@@ -0,0 +1,32 @@
+function encriptar() {
+    const inputText = document.getElementById('inputText').value;
+    let encryptedText = inputText.replace(/e/g, 'enter')
+                                 .replace(/i/g, 'imes')
+                                 .replace(/a/g, 'ai')
+                                 .replace(/o/g, 'ober')
+                                 .replace(/u/g, 'ufat');
+    document.getElementById('outputText').value = encryptedText;
+}
+
+function desencriptar() {
+    const inputText = document.getElementById('inputText').value;
+    let decryptedText = inputText.replace(/enter/g, 'e')
+                                 .replace(/imes/g, 'i')
+                                 .replace(/ai/g, 'a')
+                                 .replace(/ober/g, 'o')
+                                 .replace(/ufat/g, 'u');
+    document.getElementById('outputText').value = decryptedText;
+}
+function copiarTexto() {
+    const outputText = document.getElementById('outputText');
+    outputText.select();
+    outputText.setSelectionRange(0, 99999); // Para dispositivos móviles
+
+    navigator.clipboard.writeText(outputText.value)
+        .then(() => {
+            alert("Texto copiado al portapapeles!");
+        })
+        .catch(err => {
+            console.error("Error al copiar el texto: ", err);
+        });
+}
